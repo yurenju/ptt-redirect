fd4f21bc5caee38c8f49966c346a123294ca7046
小克 <goodjack@users.noreply.github.com>
2018-08-02 23:03:29 +0800

    Add support for moptt.tw (#7)
    * Add support for moptt.tw
    * Add a regular expression test for moptt.tw before redirecting

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 這是一個將一些直接轉載 ptt.cc 的網站重導回 ptt.cc，目前支援的網站如下：
 * disp.cc
 * webptt.com
+* moptt.tw
 
 如果想加入其他網站請發 pull request。
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -3,7 +3,7 @@
 var selector = document.location.pathname.startsWith('/m/') ? '.fgG0' : '.record';
 var elements = document.querySelectorAll(selector);
 if (elements.length) {
-  var line = Array.prototype.filter.call(elements,function(record) {
+  var line = Array.prototype.filter.call(elements, function (record) {
     return record.textContent.indexOf('文章網址') !== -1;
   })[0].textContent;
   var matched = line.match(/(https*:\/\/[\w\.\/]+)/);
@@ -11,9 +11,17 @@ if (elements.length) {
     window.location.href = matched[1];
   }
 }
+
 if (window.location.hostname == "webptt.com") {
   var search = window.location.search.split("n=");
   if (search.length > 1) {
     window.location.href = "https://www.ptt.cc/" + search[1];
+  }
+}
+
+if (window.location.hostname == "moptt.tw") {
+  let regexp = /\/p\/([^\.]+)\.(.*)/;
+  if (regexp.test(window.location.pathname)) {
+    window.location.href = window.location.pathname.replace(regexp, "https://www.ptt.cc/bbs/$1/$2.html");
   }
 }
