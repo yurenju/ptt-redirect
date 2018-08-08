dfbdee2325ae2f84021017b71de39207af6d332b
小克 <mirumodepon@gmail.com>
2018-08-08 23:55:19 +0800

    Add support for 20 websites

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
 # ptt-redirect
 
 這是一個將一些直接轉載 ptt.cc 的網站重導回 ptt.cc，目前支援的網站如下：
+* 94ptt.com
+* btrend.amassly.com
 * disp.cc
-* webptt.com
 * moptt.tw
+* ptt.av01.online
+* ptt.jimpop.org
+* ptt.social
+* ptt.techroomage.com
+* pttent.com
+* pttgame.com
+* pttgossip.com
+* ptthito.com
+* pttlocal.com
+* pttman.com
+* pttread.com
+* pttsport.com
+* pttweb.tw
+* sexptt.tk
+* webptt.com
+* webptt.xyz
+* www.laiptt.online
+* www.pttdata.com
+* www.ucptt.com
 
 如果想加入其他網站請發 pull request。
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -12,6 +12,51 @@ if (elements.length) {
   }
 }
 
+if (window.location.hostname == "btrend.amassly.com") {
+  var selector = '.f2';
+  var elements = document.querySelectorAll(selector);
+  if (elements.length) {
+    var line = Array.prototype.filter.call(elements, function (record) {
+      return record.textContent.indexOf('文章網址') !== -1;
+    })[0].textContent;
+    var matched = line.match(/(https*:\/\/[\w\.\/-]+)/);
+    if (matched) {
+      window.location.href = matched[1];
+    }
+  }
+}
+
+if (window.location.hostname == "ptt.jimpop.org") {
+  var selector = '.col-lg-10.col-lg-offset-2';
+  var elements = document.querySelectorAll(selector);
+  if (elements.length) {
+    var line = Array.prototype.filter.call(elements, function (record) {
+      return record.textContent.indexOf('連結') !== -1;
+    })[0].innerHTML;
+    var matched = line.match(/(https*:\/\/[\w\.\/-]+)/);
+    if (matched) {
+      window.location.href = matched[1];
+    }
+  }
+}
+
+if (window.location.hostname == "www.laiptt.online") {
+  var selector = '.post_body>a';
+  var elements = document.querySelectorAll(selector);
+  if (elements.length) {
+    var line = Array.prototype.filter.call(elements, function (record) {
+      var target = record.nextElementSibling.nextElementSibling;
+      if (target == null || target.name == null) {
+        return false;
+      }
+      return record.nextElementSibling.nextElementSibling.name === 'push-1';
+    })[0].innerHTML;
+    if (line) {
+      window.location.href = line;
+    }
+  }
+}
+
 if (window.location.hostname == "webptt.com") {
   var search = window.location.search.split("n=");
   if (search.length > 1) {
@@ -19,9 +64,60 @@ if (window.location.hostname == "webptt.com") {
   }
 }
 
+if (window.location.hostname == "www.ucptt.com") {
+  var regexp = /\/article\/([^\/]+)\/(.+)\/(.+)$/;
+  if (regexp.test(window.location.pathname)) {
+    window.location.href = window.location.pathname.replace(regexp, "https://www.ptt.cc/bbs/$1/M.$2.A.$3.html");
+  }
+}
+
 if (window.location.hostname == "moptt.tw") {
-  let regexp = /\/p\/([^\.]+)\.(.+)/;
+  var regexp = /\/p\/([^\.]+)\.(.+)/;
   if (regexp.test(window.location.pathname)) {
     window.location.href = window.location.pathname.replace(regexp, "https://www.ptt.cc/bbs/$1/$2.html");
+  }
+}
+
+if (window.location.hostname == "pttgossip.com") {
+  var regexp = /.*\/(([^\.\/]+\.){3}[^\.\/]+)\.html/;
+  if (regexp.test(window.location.pathname)) {
+    window.location.href = window.location.href.replace(regexp, 'https://www.ptt.cc/bbs/Gossiping/$1.html');
+  }
+}
+
+var caseBoardWithDots = [
+  '94ptt.com',
+  'ptt.av01.online',
+  'ptt.social',
+  'ptt.techroomage.com',
+  'pttent.com',
+  'pttgame.com',
+  'pttlocal.com',
+  'pttman.com',
+  'pttsport.com',
+  'sexptt.tk',
+  'webptt.xyz',
+  'www.pttdata.com'
+];
+
+if (caseBoardWithDots.indexOf(window.location.hostname) > -1) {
+  var regexp = /.*\/([^\/]+)\/(([^\.\/]+\.){3}[^\.\/]+)($|\.html$)/;
+  if (regexp.test(window.location.href)) {
+    window.location.href = window.location.href.replace(regexp, 'https://www.ptt.cc/bbs/$1/$2.html');
+  }
+}
+
+var caseBoardWithHyphen = [
+  'pttweb.tw',
+  'pttread.com',
+  'ptthito.com'
+];
+
+if (caseBoardWithHyphen.indexOf(window.location.hostname) > -1) {
+  var regexp = /.*\/([^\/]+)\/(([^-\/]+-){3}[^\.\/]+)($|\.html$|\/$)/;
+  if (regexp.test(window.location.href)) {
+    var board = window.location.pathname.replace(regexp, '$1');
+    var postId = window.location.pathname.replace(regexp, '$2').replace(/-/g, '.').toUpperCase();
+    window.location.href = 'https://www.ptt.cc/bbs/' + board + '/' + postId + '.html';
   }
 }
